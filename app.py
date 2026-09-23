62b0b85daa86e8fff95d250fbe26c279a04230c7
Khushi Swapnil Dhok <khushidhok99@gmail.com>
2026-09-23 13:51:16 +0530

    Refactor app.py for AMP prediction functionality

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if sequence:
         ax.set_xticklabels(normalized_df.columns, rotation=45, ha='right')
         ax.legend()
         ax.set_ylabel("Normalized value (0-1)")
-        st.pyplot(fig)
+        
         col1, col2 = st.columns([1, 2])
 
         with col1:
